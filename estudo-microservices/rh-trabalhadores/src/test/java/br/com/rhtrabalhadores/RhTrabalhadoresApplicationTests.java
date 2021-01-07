5eca2cc0de6c137e4142beab73d9a351f7873c31
Matheus Glauber Rodrigues Jordão <glauber.jordao1995@outlook.com>
2021-01-06 22:08:31 -0300

    Initial commit

---
+++ estudo-microservices/rh-trabalhadores/src/test/java/br/com/rhtrabalhadores/RhTrabalhadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rhtrabalhadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhTrabalhadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
