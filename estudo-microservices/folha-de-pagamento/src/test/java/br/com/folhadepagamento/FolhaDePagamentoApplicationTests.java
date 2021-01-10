54d7bd95d732bf763b633950e4fe30b1dc0c40cc
Matheus Glauber Rodrigues Jordão <glauber.jordao1995@outlook.com>
2021-01-10 16:01:53 -0300

    Projeto folha-de-pagamento criado

---
+++ estudo-microservices/folha-de-pagamento/src/test/java/br/com/folhadepagamento/FolhaDePagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.folhadepagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolhaDePagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
