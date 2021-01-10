4d2593a804639a933f4b09703a1e7978f812ed25
Matheus Glauber Rodrigues Jordão <glauber.jordao1995@outlook.com>
2021-01-10 15:49:05 -0300

    Configurado o application.properties para o banco em memoria h2, e alteração da porta (temporariamente) para a 8095, inserção de massa de teste

---
+++ estudo-microservices/rh-trabalhadores/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO TRABALHADOR (NOME, DIARIA) values ('Matheus Glauber', 175);
+INSERT INTO TRABALHADOR (NOME, DIARIA) values ('Laissa Albuquerque', 85);
+INSERT INTO TRABALHADOR (NOME, DIARIA) values ('Lucas Desenrolado', 125);
+INSERT INTO TRABALHADOR (NOME, DIARIA) values ('Marta Dayanne', 50);
