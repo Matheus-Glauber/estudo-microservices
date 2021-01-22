45b53b91f2f0a457ecc7d2f3e09448524f1c4828
Matheus Glauber Rodrigues Jordão <glauber.jordao1995@outlook.com>
2021-01-21 21:20:51 -0300

    Correções para o feignClient

--- estudo-microservices/folha-de-pagamento/src/main/java/br/com/folhadepagamento/services/PagamentoService.java
+++ estudo-microservices/folha-de-pagamento/src/main/java/br/com/folhadepagamento/services/PagamentoService.java
@@ -1,12 +1,7 @@
 package br.com.folhadepagamento.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import br.com.folhadepagamento.entities.Pagamento;
 import br.com.folhadepagamento.entities.Trabalhador;
@@ -14,8 +9,6 @@ import br.com.folhadepagamento.feignclients.TrabalhadorFeignClients;
 
 @Service
 public class PagamentoService {
-	
-	private static final String GETTRABALHADORID= "/trabalhadores/{id}";
 	
 	@Autowired
 	private TrabalhadorFeignClients trabalhadorFeignClients;
